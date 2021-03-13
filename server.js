99fa6a66ff713c94af1ecfb9534d275f3f888967
rasputinforever <erik.justin.portillo@gmail.com>
2021-03-13 12:31:19 -0800

    some pseudocode generated

---
+++ server.js
@@ -0,0 +1,26 @@
+// TODOs
+// Create Mongoose Model
+
+    // What all information is needed in our database? Check the public files for specifics. 
+    // Workout {
+        // type (cardio) string x
+        // name string x
+        // distance number
+        // duration number
+
+        // type (resistance) string x
+        // name string x
+        // weight number
+        // sets number
+        // reps number
+        // duration number
+    // }
+
+// create routes
+    // we have given files for what to serve, the question is what format do these need to be in. Inspect the public files for what is expected.
+    // /api/workouts
+        // GET, PUT, POST, 
+    // /api/workouts/range
+        // GET
+
+// create server
