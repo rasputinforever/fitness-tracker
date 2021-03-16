3d3f253f6fe281d3ba37b16f0aafbc23749a3da7
rasputinforever <erik.justin.portillo@gmail.com>
2021-03-16 11:13:06 -0700

    data structure seems to be working

--- routes/routes.js
+++ routes/routes.js
@@ -4,28 +4,28 @@ const path = require('path');
 
 module.exports = function(app) {
 
-  // app.get('/api/workouts', (req, res) => {
-  //   Workouts.find({})
-  //   .then(data => {
-  //     res.json(data)
-  //   })
+  app.get('/api/workouts', (req, res) => {
+    Workouts.find({})
+    .then(data => {
+      console.log(data[data.length - 1])
+      res.json(data)
+    })
 
+  })
+
+  // app.get('/api/workouts', (req, res) => {
+  //   Workouts.aggregate([
+  //     {$exercises : {}},
+  //     {$group: {
+  //         _id : "$_id", 
+  //         length: {$sum : "$duration"}
+  //     }}
+  // ], function(err, result){
+  //     console.log(result);
+  //     return (result)
   // })
 
-  app.get('/api/workouts', (req, res) => {
-    Workouts.aggregate([
-      {$exercises : {}},
-      {$group: {
-          _id : "$_id", 
-          length: {$sum : "$duration"}
-      }}
-  ], function(err, result){
-      console.log(result);
-      return (result)
-  })
-  
-
-  })
+  // })
 
   app.post('/api/workouts', (req, res) => {
 
