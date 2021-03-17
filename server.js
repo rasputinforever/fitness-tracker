3f0dc4c22d397f4f0c0bfff78ae520474731cc68
rasputinforever <erik.justin.portillo@gmail.com>
2021-03-16 23:44:16 -0700

    fixed location of db

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use('/', express.static('public'));
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true });
 
 // routes
 require('./routes/routes-api.js')(app)
